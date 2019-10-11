91d140ef526f3397e10d2d2a55c921de04cdca60
zhan430 <zhan430@usc.edu>
2019-10-11 20:29:54 +0800

    correct app.py log format

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ def predict():
     # get an output prediction from the pretrained model, clf
     prediction = list(clf.predict(scaled_payload))
     # TO DO:  Log the output prediction value
-    LOG.info(f"output prediction: \n %s" ,prediction)
+    LOG.info("output prediction: \n %s" ,prediction)
     return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
